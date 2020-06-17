5b73131af23847df3a0d854e43b1f386d1b1f8dd
Cathy D'Onofrio <catd825@gmail.com>
2020-06-17 21:56:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,8 +27,8 @@ new_cart=[]
   cart.each_with_index do |item, index|  #iterate over the cart for the item and index they are located at 
     item_name = cart[index][:item] #create variable for the item name - uses the current index to select the hash of 'cart', returns curent :item value
     found_item = find_item_by_name_in_collection(item_name,new_cart) #create found_item variable - uses previously created method to take in the item name of current iteration and the newly created array, to return hash which item belongs to
-    if found_item #if the found item is true or false (not nil)
-      found_item[:count] +=1  
+    if found_item #if the found item exists
+      found_item[:count] +=1  #add to count
     else
       cart[index][:count] = 1 #if value does not exist yet and is therefore nil, count starts at 1
       new_cart << cart[index] #push the revised array with count into the new array
