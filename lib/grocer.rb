d15d5bcebc876ba12cbc261aad4d946d57140f9a
Cathy D'Onofrio <catd825@gmail.com>
2020-06-17 21:47:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -25,10 +25,10 @@ def consolidate_cart(cart)
 new_cart=[]
   
   cart.each_with_index do |item, index|  #iterate over the cart for the item and index they are located at 
-    item_name = cart[index][:item] #create variable for the item name - uses the current index to select the hash of 'cart', returns :item key value
-    found_item = find_item_by_name_in_collection(item_name,new_cart)
-    if found_item
-      found_item[:count] +=1
+    item_name = cart[index][:item] #create variable for the item name - uses the current index to select the hash of 'cart', returns curent :item value
+    found_item = find_item_by_name_in_collection(item_name,new_cart) #create found_item variable - uses previously created method to take in the item name of current iteration and the newly created array 
+    if found_item #if the found item is true or false (not nil)
+      found_item[:count] +=1  
     else
       cart[index][:count] = 1
       new_cart << cart[index]
