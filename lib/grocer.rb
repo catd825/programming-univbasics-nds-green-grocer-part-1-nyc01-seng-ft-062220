0cdcec1f3fe494e365b3c64cae67c5f289e0d461
Cathy D'Onofrio <catd825@gmail.com>
2020-06-17 18:54:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,48 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
 
-end
-
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-end
-
-
+  final_hash=nil
   
+    collection.each do |hashes| #iterate over array of hashes that contain keys and values for each grocery item
+      hashes.each do |item, item_name| #iterate over each hash to return key and value for each item
+        if name == item_name #if the item passed through matches an item in the car
+          final_hash = hashes #assign the hash it exists in
+        end
+      end
+    end
+  final_hash #return hash that the item exists in
+end
+
+
+#inputs are a name (string) and collection (array of hashes).  
+#need input to match the value within a hash within an array
+#return value should be the hash the value exists in
+
+
+
+def consolidate_cart(cart) #pass through array of hashes, which include item, price, and clearance
+
+cart_with_count = []
+
+
+  cart.each_with_index do |hash, index|
+    hash.each do |hash_key, hash_value|
+      
+
+      binding.pry
+
+    end
+  end
+
+cart_with_count
+
+end
+
+#we want to return array of hashes along with COUNT
+# [iterate over each{k=>v, k=>v, k=>v} {k=>v, k=>v, k=>v} {k=>v, k=>v, k=>v}]
+# iterate over first key value pair :item
+# count occurrences of :item's value 
+# if 
+
+
